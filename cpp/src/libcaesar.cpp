910094dff724254dc45add7a0a789380b60fb811
George Lee Ge <hello@georgeleege.com>
2021-08-27 18:04:41 +0000

    Add tests and implementation for caesar shift

--- cpp/src/libcaesar.cpp
+++ cpp/src/libcaesar.cpp
@@ -1,4 +1,12 @@
 #include "libcaesar.h"
 #include <string>
 
-std::string caesar_shift(int key, std::string text) { return "Hail Caesar"; }
+std::string caesar_shift(int key, std::string text) {
+  std::string output = "";
+  key %= 95;
+  for (auto i = 0; i < text.size(); ++i) {
+    char shifted = (text[i] - ' ' + key) % 95 + ' ';
+    output += shifted;
+  }
+  return output;
+}
--- cpp/tests/libcaesar_test.cpp
+++ cpp/tests/libcaesar_test.cpp
@@ -4,3 +4,24 @@
 TEST_CASE("Caesar shift of 0 returns same", "[caesar]") {
   REQUIRE(caesar_shift(0, "Hello World!") == "Hello World!");
 }
+
+TEST_CASE("Caesar shift of 1", "[caesar]") {
+  REQUIRE(caesar_shift(1, "Hello World!") == "Ifmmp!Xpsme\"");
+}
+
+TEST_CASE("Caesar shift of -1", "[caesar]") {
+  REQUIRE(caesar_shift(-1, "Ifmmp!Xpsme\"") == "Hello World!");
+}
+
+// There are 95 printable ASCII characters
+TEST_CASE("Caesar shift of 95", "[caesar]") {
+  REQUIRE(caesar_shift(95, "Hello World!") == "Hello World!");
+}
+
+TEST_CASE("Caesar shift of 96", "[caesar]") {
+  REQUIRE(caesar_shift(96, "Hello World!") == "Ifmmp!Xpsme\"");
+}
+
+TEST_CASE("Caesar shift of -96", "[caesar]") {
+  REQUIRE(caesar_shift(-96, "Ifmmp!Xpsme\"") == "Hello World!");
+}

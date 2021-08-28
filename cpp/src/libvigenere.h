efc5e8daf77d943dd9f48131ae742b6dca01d903
George Lee Ge <hello@georgeleege.com>
2021-08-28 03:09:21 +0000

    Add header guard to libvigenere.h

--- cpp/src/libvigenere.h
+++ cpp/src/libvigenere.h
@@ -1,4 +1,9 @@
+#ifndef LIBVIGENERE_H
+#define LIBVIGENERE_H
+
 #include <string>
 
 std::string vigenere_encrypt(std::string key, std::string plaintext);
 std::string vigenere_decrypt(std::string key, std::string ciphertext);
+
+#endif

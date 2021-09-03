518a7cd63f75f52e3601059c09d8109a11695ab5
George Lee Ge <hello@georgeleege.com>
2021-09-03 01:25:27 +0000

    Add initial transposition tests [cpp]

--- cpp/tests/transpose_col_test.cpp
+++ cpp/tests/transpose_col_test.cpp
@@ -38,3 +38,44 @@ TEST_CASE("Transpose col decrypt with greater width returns same", "[transpose_c
   std::string s = "Hello World!";
   REQUIRE(transpose_col_decrypt(s.size()+1, s) == "Hello World!");
 }
+
+// easy case first where s.size() % width == 0
+// nice that "Hello World!" is size 12 which divides 2,3,4,6 nicely
+
+TEST_CASE("Transpose col encrypt with width 2", "[transpose_col]") {
+  REQUIRE(transpose_col_encrypt(2, "Hello World!") == "HloWrdel ol!");
+}
+
+TEST_CASE("Transpose col decrypt with width 2", "[transpose_col]") {
+  REQUIRE(transpose_col_decrypt(2, "HloWrdel ol!") == "Hello World!");
+}
+
+TEST_CASE("Transpose col encrypt with width 3", "[transpose_col]") {
+  REQUIRE(transpose_col_encrypt(3, "Hello World!") == "HlWleoodl r!");
+}
+
+TEST_CASE("Transpose col decrypt with width 3", "[transpose_col]") {
+  REQUIRE(transpose_col_decrypt(3, "HlWleoodl r!") == "Hello World!");
+}
+
+TEST_CASE("Transpose col encrypt with width 4", "[transpose_col]") {
+  REQUIRE(transpose_col_encrypt(4, "Hello World!") == "Hore llWdlo!");
+}
+
+TEST_CASE("Transpose col decrypt with width 4", "[transpose_col]") {
+  REQUIRE(transpose_col_decrypt(4, "Hore llWdlo!") == "Hello World!");
+}
+
+TEST_CASE("Transpose col encrypt with width 6", "[transpose_col]") {
+  REQUIRE(transpose_col_encrypt(6, "Hello World!") == "HWeolrllod !");
+}
+
+TEST_CASE("Transpose col decrypt with width 6", "[transpose_col]") {
+  REQUIRE(transpose_col_decrypt(6, "HWeolrllod !") == "Hello World!");
+}
+
+
+// when s.size() % width != 0, we have to make some decisions
+// do we reject the input?
+// pad the input?
+// or account for unequal column sizes

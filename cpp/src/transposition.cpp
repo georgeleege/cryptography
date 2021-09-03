eb78062fe3a4ab83ddc2333f058f67302d14a436
George Lee Ge <hello@georgeleege.com>
2021-09-03 01:33:11 +0000

    Add code for transpose when size%width=0 [cpp]

--- cpp/src/transposition.cpp
+++ cpp/src/transposition.cpp
@@ -2,9 +2,25 @@
 #include <string>
 
 std::string transpose_col_encrypt(int width, std::string plaintext) {
-  return "Hello World!";
+  // need to handle negative widths
+  if (width == 0 || width == 1 || width >= plaintext.size()) {
+    return plaintext;
+  }
+  std::string output = "";
+  for (auto i = 0; i < width; ++i) {
+    for (auto j = i; j < plaintext.size(); j += width) {
+      output += plaintext[j];
+    }
+  }
+  return output;
+
 }
 
 std::string transpose_col_decrypt(int width, std::string ciphertext) {
-  return "Hello World!";
+  // need to handle negative widths
+  if (width == 0 || width == 1 || width >= ciphertext.size()) {
+    return ciphertext;
+  }
+  int decrypt_width = ciphertext.size() / width;
+  return transpose_col_encrypt(decrypt_width, ciphertext);
 }
